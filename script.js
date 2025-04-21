378f4054246df72f8b590fdbc6e7e146400a6e55
Sean Fenn <seanfenn99@gmail.com>
2025-04-21 17:00:37 -0400

    Add btn to generate new grids fix grid generate
    Updated grid logic to account for the 2px border that was causing issues
    keeping the div boxes within border. Added button that calls the grid
    generate funtion and clears the previous grid

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function createGrid(xAxis, yAxis){
+function createGrid(size){
     const container = document.getElementById('grid')
+    // -2 for the 2px border
+    const cellSize = (800 / size) - 2; 
+    // Clear Old Grid
+    container.innerHTML = '';
 
-    for(let i = 0; i < xAxis * yAxis; i++){
+    for(let i = 0; i < size * size; i++){
         const div = document.createElement('div');
         div.classList.add('Axis');
+        div.style.width = `${cellSize}px`;
+        div.style.height = `${cellSize}px`;
+
         container.append(div);
        
     }     
@@ -13,12 +20,20 @@ function highlight(){
 
 }
 
-createGrid(16, 16);
+createGrid(16)
+const container = document.getElementById('grid');
 
-const container = document.getElementById('grid')
-
+// turns the boxes in the grid lightblue
 container.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('Axis')){
         e.target.style.backgroundColor = 'lightblue';
     }
 });
+
+const resizeBtn = document.getElementById('resizeBtn');
+
+//allows for user input to change the grid size and clears the previous grid
+resizeBtn.addEventListener('click', (e) => {
+    let size = prompt("Enter what size grid you would like by choosing a number from 1-100");
+    createGrid(size);
+})
